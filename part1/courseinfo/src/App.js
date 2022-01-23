37b695a1cee99924c2fde172185457ca15402e6d
mbcodes <mbcodes@protonmail.com>
2022-01-22 16:25:02 -0800

    component organization

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,42 +1,8 @@
 import React from 'react';
-
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </div>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part1={props.part1} exercises1={props.exercises1} />
-      <Part part2={props.part2} exercises2={props.exercises2} />
-      <Part part3={props.part3} exercises3={props.exercises3} />
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.sum}</p>
-    </div>
-  );
-};
+import Header from '../components/Header';
+import Content from '../components/Content';
+import Part from '../components/Part';
+import Total from '../components/Total';
 
 const App = () => {
   const course = 'Half Stack application development';
