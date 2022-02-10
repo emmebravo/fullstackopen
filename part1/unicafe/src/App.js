076d33ec2b5b57c3b072f6eeb8599c02c76f249a
mbcodes <mbcodes@protonmail.com>
2022-02-09 16:26:28 -0800

    update

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Header from './components/Header';
 import Button from './components/Button';
 import Statistics from './components/Statistics';
