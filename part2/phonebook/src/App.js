566945480a33921f099b626b976e2b5bb84ac613
mbcodes <mbcodes@protonmail.com>
2022-02-19 13:00:15 -0800

    part deux phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,12 +3,19 @@ import { useState } from 'react';
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addName = (event) => {
     event.preventDefault();
     const nameObject = {
       name: newName,
     };
+
+    const personDuplicate = persons.map((human) => human.name === newName);
+    if (personDuplicate) {
+      window.alert(`${newName} is already added to phonebook`);
+    }
+
     setPersons(persons.concat(nameObject));
     setNewName('');
   };
@@ -24,6 +31,9 @@ const App = () => {
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleAddName} />
+        </div>
+        <div>
+          phonebook: <input />
         </div>
         <div>
           <button type='submit'>add</button>
