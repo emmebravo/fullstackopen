ae3c219ec98b9480781f5811545365487b14da38
mbcodes <mbcodes@protonmail.com>
2022-07-20 23:01:34 -0700

    deleted files

--- part2/countries/src/components/FindCountries.js
+++
@@ -1,9 +0,0 @@
-const FindCountries = ({ search, onChange }) => {
-  return (
-    <>
-      find countries <input value={search} onChange={onChange} />
-    </>
-  );
-};
-
-export default FindCountries;
